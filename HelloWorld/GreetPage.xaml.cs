1f5c2b32e76d898bf6ec0ed52eb8e30e3f4476f1
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-08 14:18:27 +0200

    Data Binding

--- HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/GreetPage.xaml.cs
@@ -8,12 +8,7 @@ namespace HelloWorld
         public GreetPage()
         {
             InitializeComponent();
-            hello_slider.Value = 0.5;
-        }
-
-        void Slider_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
-        {
-            label.Text = string.Format("Value : {0:F2}", e.NewValue);
+            slider.Value = 0.5;
         }
 
     }
