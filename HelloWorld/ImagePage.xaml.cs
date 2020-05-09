15674533933dd10e5490da10e4064d06d918c74f
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-09 12:43:05 +0200

    Implemented embedded images

--- HelloWorld/ImagePage.xaml.cs
+++ HelloWorld/ImagePage.xaml.cs
@@ -11,11 +11,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            var imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/sports/7/") };
-            imageSource.CachingEnabled = false;
-            imageSource.CacheValidity = TimeSpan.FromHours(1);
-
-            image.Source = imageSource;
+            image.Source = ImageSource.FromResource("HelloWorld.Images.background.jpg");
         }
     }
 }
