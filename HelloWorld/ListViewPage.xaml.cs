74d6f21e47b50104d433cfb36bc96082375ae2f4
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-09 22:45:46 +0200

    Implemented context actions (Swipe right to call/delete)

--- HelloWorld/ListViewPage.xaml.cs
+++ HelloWorld/ListViewPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using HelloWorld.Models;
 using Xamarin.Forms;
 
@@ -7,24 +8,31 @@ namespace HelloWorld
 {
     public partial class ListViewPage : ContentPage
     {
+        private ObservableCollection<Contact> _contacts;
         public ListViewPage()
         {
             InitializeComponent();
-            var names = new List<ContactGroup>
+            _contacts = new ObservableCollection<Contact>
             {
-                new ContactGroup("U", "U"){
-                    new Contact {Name= "Ubaid", ImageUrl = "http://lorempixel.com/100/100/people/1/"}
-                },
-                new ContactGroup("S", "S"){
-                    new Contact {Name= "Sibgha", ImageUrl = "http://lorempixel.com/100/100/people/2/", Status="Hey Sibgha, Lets talk"}
-
-                },
-                new ContactGroup("A", "A"){
-                    new Contact {Name= "Ali", ImageUrl = "http://lorempixel.com/100/100/people/3/", Status="Moderator"}
-
-                }
+                new Contact {Name= "Ubaid", ImageUrl = "http://lorempixel.com/100/100/people/1/"},
+                new Contact {Name= "Sibgha", ImageUrl = "http://lorempixel.com/100/100/people/2/", Status="Hey Sibgha, Lets talk"},
+                new Contact {Name= "Ali", ImageUrl = "http://lorempixel.com/100/100/people/3/", Status="Moderator"},
             };
-            listView.ItemsSource = names;
+            listView.ItemsSource = _contacts;
+        }
+
+        void Call_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            var contact = menuItem.CommandParameter as Contact;
+
+            DisplayAlert("Call", contact.Name, "Ok");
+        }
+
+        void Delete_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+            _contacts.Remove(contact);
         }
     }
 }
