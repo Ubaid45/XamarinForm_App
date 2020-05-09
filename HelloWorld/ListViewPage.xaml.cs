8dd398deede3bb1d5879b1a5a1a75b730e3494ee
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-09 22:54:50 +0200

    Implemented pull to refresh

--- HelloWorld/ListViewPage.xaml.cs
+++ HelloWorld/ListViewPage.xaml.cs
@@ -9,15 +9,20 @@ namespace HelloWorld
     public partial class ListViewPage : ContentPage
     {
         private ObservableCollection<Contact> _contacts;
-        public ListViewPage()
+
+        ObservableCollection<Contact> GetContacts()
         {
-            InitializeComponent();
-            _contacts = new ObservableCollection<Contact>
+            return new ObservableCollection<Contact>
             {
                 new Contact {Name= "Ubaid", ImageUrl = "http://lorempixel.com/100/100/people/1/"},
                 new Contact {Name= "Sibgha", ImageUrl = "http://lorempixel.com/100/100/people/2/", Status="Hey Sibgha, Lets talk"},
                 new Contact {Name= "Ali", ImageUrl = "http://lorempixel.com/100/100/people/3/", Status="Moderator"},
             };
+        }
+        public ListViewPage()
+        {
+            InitializeComponent();
+            _contacts = GetContacts();
             listView.ItemsSource = _contacts;
         }
 
@@ -33,6 +38,12 @@ namespace HelloWorld
         {
             var contact = (sender as MenuItem).CommandParameter as Contact;
             _contacts.Remove(contact);
+        }
+
+        void listView_Refreshing(System.Object sender, System.EventArgs e)
+        {
+            listView.ItemsSource = GetContacts();
+            listView.EndRefresh();
         }
     }
 }
