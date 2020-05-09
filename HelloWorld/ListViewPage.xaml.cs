fd347bf672c77abea40edf746174181ba1131601
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-09 22:28:32 +0200

    Grouping Items

--- HelloWorld/ListViewPage.xaml.cs
+++ HelloWorld/ListViewPage.xaml.cs
@@ -10,11 +10,19 @@ namespace HelloWorld
         public ListViewPage()
         {
             InitializeComponent();
-            var names = new List<Contact>
+            var names = new List<ContactGroup>
             {
-                new Contact {Name= "Ubaid", ImageUrl = "http://lorempixel.com/100/100/people/1/"},
-                new Contact {Name= "Sibgha", ImageUrl = "http://lorempixel.com/100/100/people/2/", Status="Hey Sibgha, Lets talk"},
-                new Contact {Name= "Ali", ImageUrl = "http://lorempixel.com/100/100/people/3/", Status="Moderator"},
+                new ContactGroup("U", "U"){
+                    new Contact {Name= "Ubaid", ImageUrl = "http://lorempixel.com/100/100/people/1/"}
+                },
+                new ContactGroup("S", "S"){
+                    new Contact {Name= "Sibgha", ImageUrl = "http://lorempixel.com/100/100/people/2/", Status="Hey Sibgha, Lets talk"}
+
+                },
+                new ContactGroup("A", "A"){
+                    new Contact {Name= "Ali", ImageUrl = "http://lorempixel.com/100/100/people/3/", Status="Moderator"}
+
+                }
             };
             listView.ItemsSource = names;
         }
