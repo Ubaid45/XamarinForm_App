63558c348b2bc73750fedb2990f7b54eca4f4a2b
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-09 03:59:51 +0200

    Added missing files to the source control

---
+++ HelloWorld/Exercises/Absolute Layout/AbsoluteLayout_Exercise1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld.Exercises.AbsoluteLayout
+{
+    public partial class AbsoluteLayout_Exercise1 : ContentPage
+    {
+        public AbsoluteLayout_Exercise1()
+        {
+            InitializeComponent();
+        }
+    }
+}
