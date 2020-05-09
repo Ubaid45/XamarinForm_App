ec422691bc687486fc19c62ec011fb0493a68840
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-09 03:53:29 +0200

    Implemented relative layout page in code-behind

--- HelloWorld/RelativePage.xaml.cs
+++ HelloWorld/RelativePage.xaml.cs
@@ -10,6 +10,20 @@ namespace HelloWorld
         public RelativePage()
         {
             InitializeComponent();
+
+            var layout = new RelativeLayout();
+            Content = layout;
+
+            var aquaBox = new BoxView { Color = Color.Aqua };
+            layout.Children.Add(aquaBox,
+                widthConstraint: Constraint.RelativeToParent(parent => parent.Width),
+                heightConstraint: Constraint.RelativeToParent(parent => parent.Height));
+
+            var silverBox = new BoxView { Color = Color.Silver };
+            layout.Children.Add(silverBox,
+               yConstraint: Constraint.RelativeToView(aquaBox, (RelativeLayout, element) => element.Height + 20));
+
+
         }
     }
 }
