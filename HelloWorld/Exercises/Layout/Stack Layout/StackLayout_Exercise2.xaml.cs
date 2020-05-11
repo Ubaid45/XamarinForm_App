3611e6ae9ccca69d16b8121319348b34f8e2783a
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-11 02:10:55 +0200

    Moved missing files

---
+++ HelloWorld/Exercises/Layout/Stack Layout/StackLayout_Exercise2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld.Exercises.StackLayout
+{
+    public partial class StackLayout_Exercise2 : ContentPage
+    {
+        public StackLayout_Exercise2()
+        {
+            InitializeComponent();
+        }
+    }
+}
