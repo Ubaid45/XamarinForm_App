54c993885cbbff4764f71b532fcce2c517ab9d55
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-17 00:19:13 +0200

    Added missing before style files

---
+++ HelloWorld/Exercises/Styles/Before/BeforeStylePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld.Exercises.Styles.Before
+{
+    public partial class BeforeStylePage : ContentPage
+    {
+        public BeforeStylePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
